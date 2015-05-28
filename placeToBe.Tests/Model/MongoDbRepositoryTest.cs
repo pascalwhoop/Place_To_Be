dc1ecd4078b1fffa7975f6d7bf8c34fce01862d6
Merve Nur Bas <mervenur.bas@hotmail.de>
2015-05-28 15:54:28 +0200

    Test fuer Repository

---
+++ placeToBe.Tests/Model/MongoDbRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using placeToBe;
+using placeToBe.Model.Repositories;
+using placeToBe.Model.Entities;
+using placeToBe.Model;
+
+namespace placeToBe.Tests.Model
+{
+    [TestClass]
+    public class MongoDbRepositoryTest
+    {
+        [TestMethod]
+        public void InsertAsync()
+        {
+            //Arrange
+            MongoDbRepository <Event> model = new MongoDbRepository <Event> ();
+            //Event test = ;
+
+            //Act
+            // model.InsertAsync(test);
+        }
+    }
+}
