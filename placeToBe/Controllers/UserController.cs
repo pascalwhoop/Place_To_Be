432ffd41c2653902431fe5aa8b3826d240badcb2
Mergim <malija@smail.uni-koeln.de>
2015-06-01 18:08:05 +0200

    UserController

--- placeToBe/Controllers/UserController.cs
+++ placeToBe/Controllers/UserController.cs
@@ -3,50 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using System.Web.Http;
-using placeToBe.Model;
-using placeToBe.Model.Repositories;
 using placeToBe.Model.Entities;
+using placeToBe.Model.Repositories;
+using System.Threading.Tasks;
 
 namespace placeToBe.Controllers
 {
     public class UserController : ApiController
     {
-            /*    
-        
-            // Transfer User-data
-              
-            MongoDbRepository<User> repo = new MongoDbRepository<User>();
-        
-            // GET api/User
-            public async Task<IList<User>>Get()
-            {
-                IList<User> list = await repo.GetAllAsync();
-                return list;
-            }
 
-            // GET api/User/5
-            public async Task<User> Get(Guid id)
-            {
-                User user = await repo.GetByIdAsync(id);
-                return user;
-            }
+        MongoDbRepository<User> repo = new MongoDbRepository<User>();
 
-            // POST api/User
-            public void Post([FromBody]User user)
-            {
-                repo.InsertAsync(user);
-            }
-
-            // PUT api/User/5
-            public void Put(int id, [FromBody]User user)
-            {
-                repo.UpdateAsync(User);
-            }
-        
-            */
-      
+        // GET api/User 
+        public async Task<IList<User>> Get()
+        {
+            IList<User> list = await repo.GetAllAsync();
+            return list;
         }
-     
+
+        // GET api/User/5
+        public async Task<User> Get(Guid id)
+        {
+            User User = await repo.GetByIdAsync(id);
+            return User;
+        }
+
+        // POST api/User
+        public void Post([FromBody]string value)
+        {
+            //  repo.InsertAsync(User);
+        }
+
+        // PUT api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+            // Put...
+        }
+
+        // DELETE api/User/5
+        public async void Delete(Guid id)
+        {
+            var User = await repo.GetByIdAsync(id);
+            repo.DeleteAsync(User);
+        }
     }
+}
