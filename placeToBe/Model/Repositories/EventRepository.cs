61135be1c9db7c98bde31f982cc14d6d0c4ca90f
Nils <nhartelt@smail.uni-koeln.de>
2015-06-02 16:43:46 +0200

    city

--- placeToBe/Model/Repositories/EventRepository.cs
+++ placeToBe/Model/Repositories/EventRepository.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using placeToBe.Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace placeToBe.Model.Repositories
 {
-    public class EventRepository<TEntitiy>: MongoDbRepository<Event>
+    public class EventRepository<Event>: MongoDbRepository<Event>
     {
         public void GetCityMap(City city)
         {
-
+            
         }
     }
 }
