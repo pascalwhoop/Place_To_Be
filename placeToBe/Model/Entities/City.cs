24a24dae99a0bf1fafb6d721a81c36ba2452acc7
Stephan Blumenthal <bluments@smail.uni-koeln.de>
2015-06-02 17:24:29 +0200

    City Entität update

--- placeToBe/Model/Entities/City.cs
+++ placeToBe/Model/Entities/City.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace placeToBe.Model.Entities
 {
-    public class City
+    /// <summary>
+    /// class includes a City represented by its name and coordinates 
+    /// (coordinates build quader which includes the city).
+    /// </summary>
+    public class City : EntityBase
     {
-
+        
+        public String name { get; set; }
+        public Point northWest { get; set; }
+        Point northEast { get; set; }
+        Point southEast { get; set; }
+        Point southWest { get; set; }
 
     }
+    public class Point {
 
+        public double dx { get; set; }
+        public double dy { get; set; }
+
+        public Point(double dx, double dy){
+            this.dx = dx;
+            this.dy = dy;
+         }
+    }
 }
