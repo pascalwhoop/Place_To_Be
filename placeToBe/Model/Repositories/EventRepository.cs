5e82030408220df12531513e542d162c80439930
Stephan Blumenthal <bluments@smail.uni-koeln.de>
2015-06-05 17:58:32 +0200

    Mergim City irgendwas

--- placeToBe/Model/Repositories/EventRepository.cs
+++ placeToBe/Model/Repositories/EventRepository.cs
@@ -14,19 +14,20 @@ namespace placeToBe.Model.Repositories
     {
 
         public IMongoCollection<Event> EventCollection;
-        private Event test1;
+        private City test1;
 
         //a constructor that makes sure we have a geospherical index over our event list. 
         public EventRepository() {
             _collection.Indexes.CreateOneAsync(Builders<Event>.IndexKeys.Geo2DSphere(_ => _.locationCoordinates));
         }
 
-        public async Task<IList<Event>> GetCityMap(string place, string time)
+        public async Task<IList<City>> GetCityMap(string place, string time)
         {
+            
             var collection = GetCollection(); 
-             /*    var query = Builders<Event>.Filter.GeoWithinPolygon("Location", test1.locationCoordinates.coordinates);
-                   var result = await collection.FindAsync(query);
-            */
+            var query = Builders<City>.Filter.GeoWithinPolygon("Location", test1.polygon);
+            //var result = await collection.FindAsync(query);
+            
             return null;
         }
        
