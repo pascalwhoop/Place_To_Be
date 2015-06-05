4a30165f10bc8487804815099963619fa4520edc
Mergim <malija@smail.uni-koeln.de>
2015-06-05 16:39:14 +0200

    SearchByPlace (NochAusstehendWegenDoubleArrayAttribut)

--- placeToBe/Model/Repositories/EventRepository.cs
+++ placeToBe/Model/Repositories/EventRepository.cs
@@ -4,19 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using MongoDB.Driver;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver.Core;
 
 namespace placeToBe.Model.Repositories
 {
     public class EventRepository: MongoDbRepository<Event>
     {
+
+        public IMongoCollection<Event> EventCollection;
+        private Event test1;
+
         //a constructor that makes sure we have a geospherical index over our event list. 
         public EventRepository() {
             _collection.Indexes.CreateOneAsync(Builders<Event>.IndexKeys.Geo2DSphere(_ => _.locationCoordinates));
         }
 
-        public void GetCityMap(City city)
+        public async Task<IList<Event>> GetCityMap(string place, string time)
         {
-            
+            var collection = GetCollection(); 
+             /*    var query = Builders<Event>.Filter.GeoWithinPolygon("Location", test1.locationCoordinates.coordinates);
+                   var result = await collection.FindAsync(query);
+            */
+            return null;
         }
+       
+        public IMongoCollection<Event> GetCollection()
+        {
+            return _collection;
+        } 
+    
+
+
     }
 }
