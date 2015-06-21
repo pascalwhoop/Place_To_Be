5e350ec1b5e7d49af6dedcd4c7fc3fcdfe378eaf
Merve Nur Bas <mervenur.bas@hotmail.de>
2015-06-21 14:36:42 +0200

    App Config im Testprojekt kompatibel Web Congif im Projekt selbst Testklasse AccountServiceTest

---
+++ placeToBe.Tests/Service/AccountServiceTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using placeToBe.Services;
+using placeToBe.Model.Repositories;
+
+namespace placeToBe.Tests.Service
+{
+    [TestClass]
+    public class AccountServiceTest
+    {
+        [TestMethod]
+        public void Register()
+        {
+            //Arrange
+
+            AccountService account = new AccountService();
+            string email = "exampleplacetobe@hotmail.de";
+            string password = "exampleplacetobe123";
+
+            //Act
+
+            account.Register(email,password);
+
+            //Assert
+            //Frage, ob hier eine Asser Anweisung zugehoert
+           
+        }
+
+        [TestMethod]
+        public void Login()
+        {
+           //Arrange
+
+            AccountService account = new AccountService();
+            string email = "exampleplacetobe@hotmail.de";
+            string password = "exampleplacetobe123";
+
+            //Act 
+
+            account.Login(email, password);
+
+            //Assert
+
+            //Assert.AreEqual()
+            
+
+            /*
+            
+            //versuch mit nicht registrierter mail adresse oder passwort
+            //Test funktioniert auch mit nicht registrierten Informationen 
+
+            //Arrange
+
+            AccountService account = new AccountService();
+            string email = "exampleplacetobenicht@hotmail.de";
+            string password = "exampleplacetobe123678";
+
+
+            //Act 
+
+            account.Login(email, password);
+             */
+        }
+
+
+    }
+}
