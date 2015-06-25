0b23bc6ba92a87ddb3cc176533c3192f65391bdc
Mergim <malija@smail.uni-koeln.de>
2015-06-25 12:03:30 +0100

    confirmmailcontroller

--- placeToBe/Controllers/UserController.cs
+++ placeToBe/Controllers/UserController.cs
@@ -33,36 +33,32 @@ namespace placeToBe.Controllers
             return User;
         }
 
-        /* POST api/User
-        
-         */
+        // PUT Login - Get AuthenticationTicket for 5 minutes
+       
         public async Task<FormsAuthenticationTicket> Put([FromUri]string loginmail, [FromUri]string loginpw, [FromUri] string ueberladung)
         {
             return await user.Login(loginmail, loginpw);
         }
 
-        public void Put([FromUri]string senden){
-            user.SendActivationEmail(senden);
-        }
 
-        //Register a User with email and passwort
-        public async Task<Guid> Put([FromUri]string email, [FromUri] string passwort)
+        //Send activationsemail and register a user with email and passwort
+
+        public async Task Put([FromUri]string email, [FromUri] string passwort)
         {
-            return await user.Register(email, passwort);
+            await user.SendActivationEmail(email, passwort);
         }
 
-        //public async Task<User> Put([FromUri]string email)
-        //{
-        //    UserRepository user = new UserRepository();
-        //    return await user.GetByEmailAsync(email);
-        //}
+        public async Task Put([FromUri] string activationcode)
+        {
+            user.ConfirmEmail(activationcode);
+        }
 
-        // DELETE api/User/5
-        //ToDo
         public async void Delete(Guid id)
         {
             var User = await repo.GetByIdAsync(id);
             repo.DeleteAsync(User);
         }
+
+
     }
 }
