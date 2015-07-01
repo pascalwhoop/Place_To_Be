5266dbf66d92fe32b1a74720f25d2f8ebe91fbc9
Merve Nur Bas <mervenur.bas@hotmail.de>
2015-07-01 15:30:51 +0200

    Tests checken, ob Methode funktionieren bei einigen noch keine Asserts oder Verhalten bei ungultigem input

---
+++ placeToBe.Tests/Controllers/EventControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using placeToBe;
+using placeToBe.Controllers;
+
+namespace placeToBe.Tests.Controllers
+{
+    [TestClass]
+    public class EventControllerTest
+    {
+        [TestMethod]
+        public void getEventsByTimeAndCity()
+        {
+            //Arrange
+            EventController controller = new EventController();
+            String id = "12312312301";
+            String year = "2015";
+            String month = "06";
+            String day = "Monday";
+            String hour = "17:00";
+
+            //Act
+            controller.getEventsByTimeAndCity(id, year, month, day, hour);
+        }
+    }
+}
