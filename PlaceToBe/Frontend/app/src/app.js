a0e8ea31f9fe3873f71779f21f55fabdd5713167
Brokmeier, Pascal <Pascal.Brokmeier@opitz-consulting.com>
2015-07-01 15:11:21 +0200

    added frontend icons and changed FB in frontend a bit

--- PlaceToBe/Frontend/app/src/app.js
+++ PlaceToBe/Frontend/app/src/app.js
@@ -28,6 +28,15 @@ angular
   })
   .config(function($facebookProvider){
     $facebookProvider.setAppId('857640940981214');
+  })
+  .run(function($rootScope){
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
   });
 
 /*red
