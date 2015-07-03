6696125b354c72d6f83f6f23966f0608af5e4347
Mergim <malija@smail.uni-koeln.de>
2015-07-03 17:24:02 +0100

    Add MyBasicAuthenticationFilterClass

---
+++ PlaceToBe/Filter/MyBasicAuthenticationFilter.cs
@@ -0,0 +1,34 @@
+﻿using placeToBe.Controllers;
+using placeToBe.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+
+namespace placeToBe.Services
+{
+    public class MyBasicAuthenticationFilter : BasicAuthenticationFilter
+    {
+
+        UserController test;
+        public MyBasicAuthenticationFilter()
+        { }
+
+        public MyBasicAuthenticationFilter(bool active)
+            : base(active)
+        { active = true; }
+
+
+        protected async override Task<bool> OnAuthorizeUser(string username, string password, HttpActionContext actionContext)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)){
+                return false;
+        }else{
+                await test.Put(username, password);
+                return true;
+            }
+        }
+    }
+}
