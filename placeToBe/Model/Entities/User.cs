ad4b7bad98890c012f731b86c6bf1b185993e5b1
Mergim <malija@smail.uni-koeln.de>
2015-07-03 13:53:19 +0100

    Changes in Entity

--- placeToBe/Model/Entities/User.cs
+++ placeToBe/Model/Entities/User.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Web;
 
 namespace placeToBe.Model.Entities
 {
+    [DataContract]
     public class User : EntityBase
     {
         //The 0-parameter constructor is necessary for the structure of inheritance
@@ -18,14 +20,22 @@ namespace placeToBe.Model.Entities
             this.email = email;
             this.passwordSalt = passwordSalt;
             this.salt = salt;
+        
         }
+        [DataMember]
         public string email { get; set; }
+        [DataMember]
         public byte[] passwordSalt { get; set; }
+        [DataMember]
         public byte[] salt { get; set; }
+       [DataMember]
         // if status == true then the user is activated/confirmed, else not activated/confirmed
         public bool status { get; set; }
+       [DataMember]
         public string activationcode { get; set; }
+       [DataMember]
         public string company { get; set; }
+        [DataMember]
         public string city { get; set; }
 
     }
